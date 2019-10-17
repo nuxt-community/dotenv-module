ac790c87ed17d049b330555c2963bf78a0564020
Ricardo Gobbo de Souza <ricardogobbosouza@yahoo.com.br>
2019-10-17 04:33:17 -0300

    Improvements (#45)
    * chore: set root
    
    * test: use `@nuxtjs/module-test-utils`
    
    * docs: formatting and use `buildModules`
    
    * Update README.md
    
    * Update package.json

--- test/basic.test.js
+++ test/basic.test.js
@@ -1,14 +1,10 @@
-const { Nuxt, Builder } = require('nuxt-edge')
-const getPort = require('get-port')
+const { setup, loadConfig, get } = require('@nuxtjs/module-test-utils')
 
 describe('basic', () => {
   let nuxt
 
   beforeAll(async () => {
-    nuxt = new Nuxt(require('./fixture/basic/nuxt.config'))
-    await nuxt.ready()
-    await new Builder(nuxt).build()
-    await nuxt.listen(await getPort())
+    ({ nuxt } = await setup(loadConfig(__dirname, 'basic')))
   }, 60000)
 
   afterAll(async () => {
@@ -16,19 +12,19 @@ describe('basic', () => {
   })
 
   test('variables from the .env file should have been loaded', async () => {
-    const { html } = await nuxt.renderRoute('/')
+    const html = await get('/')
     expect(html).toContain('foo: bar')
     expect(html).toContain('bar: baz')
     expect(html).toContain('baz: foo')
   })
 
   test('variables from the .env file should NOT override existing values from nuxt.config.js', async () => {
-    const { html } = await nuxt.renderRoute('/')
+    const html = await get('/')
     expect(html).not.toContain('foo: baz')
   })
 
   test('when an only array is passed, the variables not included should not be loaded', async () => {
-    const { html } = await nuxt.renderRoute('/')
+    const html = await get('/')
     expect(html).toContain('more1: yep')
     expect(html).not.toContain('more2: nope')
   })
